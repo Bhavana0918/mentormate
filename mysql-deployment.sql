b6dae4536f1ce1a3ab554f18aa7c15b97bbc9c36
Bhavana Doddabasappanavar <Bhavana.D@valtech.com>
2024-07-01 23:18:30 +0530

    add

--- mysql-deployment.sql
+++ mysql-deployment.sql
@@ -1,7 +1,7 @@
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  name: mysql
+  name: mysql-deployment
 spec:
   replicas: 1
   selector:
@@ -13,23 +13,23 @@ spec:
         app: mysql
     spec:
       containers:
-      - name: mysql
-        image: mysql:8.1
-        ports:
-        - containerPort: 3306
-        env:
-        - name: MYSQL_DATABASE
-          value: mentormate
-        - name: MYSQL_USER
-          value: admin
-        - name: MYSQL_PASSWORD
-          value: password
-        - name: MYSQL_ROOT_PASSWORD
-          value: password
-        volumeMounts:
-        - name: mysql-storage
-          mountPath: /var/lib/mysql
+        - name: mysql
+          image: mysql:8.1
+          env:
+            - name: MYSQL_ROOT_PASSWORD
+              value: password
+            - name: MYSQL_DATABASE
+              value: mentormate
+            - name: MYSQL_USER
+              value: admin
+            - name: MYSQL_PASSWORD
+              value: password
+          ports:
+            - containerPort: 3306
+          volumeMounts:
+            - name: mysql-persistent-storage
+              mountPath: /var/lib/mysql
       volumes:
-      - name: mysql-storage
-        persistentVolumeClaim:
-          claimName: mysql-pvc
+        - name: mysql-persistent-storage
+          persistentVolumeClaim:
+            claimName: mysql-pv-claim
