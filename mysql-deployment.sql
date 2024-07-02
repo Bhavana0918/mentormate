a77ef22ebf5e04cdcc74f04c8f380d635d5f3257
Bhavana0918 <166234807+Bhavana0918@users.noreply.github.com>
2024-07-02 16:09:43 +0530

    Update mysql-deployment.sql

--- mysql-deployment.sql
+++ mysql-deployment.sql
@@ -1,35 +1,37 @@
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  name: mysql-deployment
+  name: mysql
 spec:
-  replicas: 1
   selector:
     matchLabels:
       app: mysql
+  strategy:
+    type: Recreate
   template:
     metadata:
       labels:
         app: mysql
     spec:
       containers:
-        - name: mysql
-          image: mysql:8.1
-          env:
-            - name: MYSQL_ROOT_PASSWORD
-              value: password
-            - name: MYSQL_DATABASE
-              value: mentormate
-            - name: MYSQL_USER
-              value: admin
-            - name: MYSQL_PASSWORD
-              value: password
-          ports:
-            - containerPort: 3306
-          volumeMounts:
-            - name: mysql-persistent-storage
-              mountPath: /var/lib/mysql
-      volumes:
+      - image: 122238031788.dkr.ecr.ap-south-1.amazonaws.com/mysql:8.1
+        name: mysql
+        env:
+        - name: MYSQL_DATABASE
+          value: "mentormate"
+        - name: MYSQL_USER
+          value: "admin"
+        - name: MYSQL_PASSWORD
+          value: "password"
+        - name: MYSQL_ROOT_PASSWORD
+          value: "password"
+        ports:
+        - containerPort: 3306
+          name: mysql
+        volumeMounts:
         - name: mysql-persistent-storage
-          persistentVolumeClaim:
-            claimName: mysql-pv-claim
+          mountPath: /var/lib/mysql
+      volumes:
+      - name: mysql-persistent-storage
+        persistentVolumeClaim:
+          claimName: mysql-pvc
